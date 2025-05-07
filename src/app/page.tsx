0b40aa11b076e5a52b546e3d39d83b1a5dcd7a50
PragadeeshG4102 <pragadeeshbillu4102@gmail.com>
2025-05-07 12:34:23 +0530

    feat: featurecard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import FeatureCard from "@/components/feature-card/feature-card";
 import HeroSection from "@/components/hero-section/hero-section";
 
 export default function Home() {
   return (
     <main className="max-w-screen-3xl mx-auto">
       <HeroSection /> 
+      <FeatureCard />
     </main>
   );
 }
--- src/assets/png/images.ts
+++ src/assets/png/images.ts
@@ -1,11 +1,23 @@
 import heroSplash from '@/assets/png/hero-splash.png';
 import heromuscle from '@/assets/png/muscle.png';
+import tea from '@/assets/png/feature-tea.png';
+import leaf from '@/assets/png/feature-leaf.png';
+import office from '@/assets/png/feature-office.png';
+import heroleft from '@/assets/png/hero-art-left.png';
+import heroright from '@/assets/png/hero-art-right.png';
 
 export const images = {
   heroSection: {
     heroSplash,
+    heroleft,
+    heroright,
   },
   heroframe: {
     heromuscle,
+  },
+  feature: {
+    tea,
+    leaf,
+    office
   }
 } as const;
