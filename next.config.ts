647e89036a484a76deb5689ff21bfc0b1a28dc92
chhroot <amruthkiran47@gmail.com>
2025-05-14 17:17:07 +0530

    fix: svgr fix2

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,19 @@ const nextConfig: NextConfig = {
           loader: '@svgr/webpack',
           options: {
             icon: true,
+            svgoConfig: {
+              plugins: [
+                {
+                  name: 'preset-default',
+                  params: {
+                    overrides: {
+                      removeViewBox: false,
+                    },
+                  },
+                },
+                'removeDimensions',
+              ],
+            },
           },
         },
       ],
