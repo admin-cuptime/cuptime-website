dad9bc878542382c572571297d5f083ff0156435
PragadeeshG4102 <pragadeeshbillu4102@gmail.com>
2025-05-23 12:15:01 +0530

    feat: delivery page created

---
+++ src/components/delivery-content/delivery-section-content.ts
@@ -0,0 +1,93 @@
+export const deliveryData = [
+  {
+    title: "1. Delivery Areas:",
+    points: [
+      "We deliver to areas in Madurai & Coimbatore.",
+    ],
+  },
+  {
+    title: "2. Delivery Times:",
+    points: [
+      "Our standard delivery times are Monday to Sunday, 10am to 11am in the morning & 4:30pm to 5pm in the evening.",
+      "Note - Timings may differ according to circumstances.",
+    ],
+  },
+  {
+    title: "3. Delivery Methods:",
+    points: [
+      "We use our own delivery vehicles & persons for delivery.",
+    ],
+  },
+  {
+    title: "4. Delivery Charges:",
+    points: [
+      "Our delivery charges are applied if needed during the regular billing.",
+    ],
+  },
+  {
+    title: "5. Minimum Order Requirements:",
+    points: [
+      "We have a minimum order requirement of 500ml.",
+    ],
+  },
+  {
+    title: "6. Order Cut-Off Times:",
+    points: [
+      "Orders must be placed by at least a day before.",
+    ],
+  },
+  {
+    title: "7. Delivery Tracking:",
+    points: [
+      "We provide tracking information for all deliveries through our Mobile App or QR delivery code.",
+    ],
+  },
+  {
+    title: "8. Failed Delivery Attempts:",
+    points: [
+      "If we are unable to deliver an order due to special circumstances, we will share the information prior.",
+    ],
+  },
+  {
+    title: "9. Damaged or Missing Items:",
+    points: [
+      "If an order is missing or the product received is damaged, please contact us anytime.",
+    ],
+  },
+  {
+    content: "Special Delivery Instructions",
+    title: "",
+    points: [
+    ],
+  },
+  {
+    title: "1. Special Delivery Requests:",
+    points: [
+      "If you have special delivery instructions we will appreciate it if mentioned during the booking.",
+    ],
+  },
+  {
+    title: "2. Delivery to Multiple Addresses:",
+    points: [
+      " If you need to deliver to multiple addresses, please inform in prior.",
+    ],
+  },
+  {
+    content:"Delivery Restrictions",
+    title: "",
+    points: [
+    ],
+  },
+  {
+    title: "1. Delivery Restrictions: ",
+    points: [
+      " We do not deliver to unusual areas without people to issue the order.",
+    ],
+  },
+  {
+    title: "2. Delivery to Businesses: ",
+    points: [
+      " We only deliver to businesses, showrooms, offices or events. Note - Not to eat at your house.",
+    ],
+  },
+];
